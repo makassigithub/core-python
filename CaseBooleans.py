a1c20cb5fdafb5086981c190830b5e29b3cda6d9
Brahima Traore <my123gate@gmail.com>
2017-09-17 21:19:21 -0400

    booleans

---
+++ CaseBooleans.py
@@ -0,0 +1,11 @@
+# Booleans can take only two values: True, False
+# zero is False, empty string is False
+#  any other value is True
+a,b = 4,5
+c = a>b
+d = a<b
+print(c,d)
+#Booleans are converted into binaries 0 and 1 for respec. False and True
+# before being passed to the OS
+cases = ['vrai', 'faux']
+print(cases[c],cases[d]) 
